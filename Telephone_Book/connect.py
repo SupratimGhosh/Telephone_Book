adc87193a625dadb1f43e0ba4bd0acc743b60ed2
SupratimGhosh <40762079+SupratimGhosh@users.noreply.github.com>
2021-05-21 22:08:42 +0530

    Update connect.py

--- Telephone_Book/connect.py
+++ Telephone_Book/connect.py
@@ -1,4 +1,4 @@
 import mysql.connector
 
 
-mydb = mysql.connector.connect(host='localhost',user='root',password='1234',database='telephone')
+mydb = mysql.connector.connect(host='localhost',user='root',password='****',database='demo_db')
